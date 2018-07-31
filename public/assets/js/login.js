95feff31c1e13acd0ed58fb002c0b84f062414ea
CaritoDasilva <caritodasilva@gmail.com>
2018-07-31 12:32:09 -0400

    se agrega botón de sign out

---
+++ public/assets/js/login.js
@@ -0,0 +1,58 @@
+initApp = function () {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var uid = user.uid;
+      var phoneNumber = user.phoneNumber;
+      var providerData = user.providerData;
+      user.getIdToken().then(function (accessToken) {
+        document.getElementById('sign-in-status').textContent = 'Signed in';
+        document.getElementById('sign-in').textContent = 'Sign out';
+        document.getElementById('account-details').textContent = JSON.stringify({
+          displayName: displayName,
+          email: email,
+          emailVerified: emailVerified,
+          phoneNumber: phoneNumber,
+          photoURL: photoURL,
+          uid: uid,
+          accessToken: accessToken,
+          providerData: providerData
+        }, null, ' ');
+      });
+    } else {
+      // User is signed out.
+      var uiConfig = {
+        signInSuccessUrl: '/',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '/',
+        // Privacy policy url.
+        privacyPolicyUrl: '/'
+      };
+
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+    }
+  }, function (error) {
+    console.log(error);
+  });
+};
+
+window.addEventListener('load', function () {
+  initApp()
+});
+
+out.addEventListener('click', () => {
+  firebase.auth().signOut();
+  login.style.display = 'block';
+});
